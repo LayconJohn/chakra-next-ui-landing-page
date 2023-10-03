cb27bb0c6602b7121874ac72b213580870a73c3a
Laycon John <laycon.gomes@tld.local>
2023-10-03 17:29:26 -0300

    feat: insert testimonials template

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 "use client"
 import HeroSection from '@/components/HeroSection'
 import Features from '@/components/Features'
+import Testimonails from '@/components/Testimonials'
 
 export default function Home() {
   return (
     <>
       <HeroSection />
       <Features />
+      <Testimonails />
     </>
   )
 }
